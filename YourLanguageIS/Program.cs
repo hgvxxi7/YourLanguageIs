05a8e7ee4fffe61e6f24774c779677bd232100ee
hgvxxi7 <hgvxxi7@gmail.com>
2018-09-06 00:34:53 +0300

    first commit

---
+++ YourLanguageIS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourLanguageIS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nНапишите, какой язык распахнул перед вами ворота в мир программирования.\n");
+
+            string user_types = Console.ReadLine();
+
+            Console.WriteLine("\nИмя вашего швейцара - " + user_types);
+            Console.ReadKey();
+        }
+    }
+}
